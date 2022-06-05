d5a053e02ea89ab7f2c67a42e7d442cf7c6f8953
Matthew Henderson <info@matthewahenderson.com>
2022-06-05 11:46:42 -0400

    Added input HTML field; added debounce and fetch data functions; added event listener for input HTML field

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Hi there!');
+// Fetch data from OMDB API endpoint
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '59ce4c45',
+            s: searchTerm
+        }
+    }); 
+    console.log(response.data);
+}
+
+// Get input HTML field
+const input = document.querySelector('input');
+
+// Debounce function for widget - makes API call with a specified delay
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay)
+    }
+};
+
+// As keys are typed call fetchData function
+const onInput = event => {
+    fetchData(event.target.value);
+}
+
+// Event listener for input HTML field
+input.addEventListener('input', debounce(onInput, 500));
