effb82d60a7aec3f85853e0ede6522c0cc74b70f
Matthew Henderson <info@matthewahenderson.com>
2022-06-06 23:22:42 -0400

    Added logic to close the dropdown and select individual movies

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
     
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+
     resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
     for (let movie of movies) {
@@ -46,6 +51,10 @@ const onInput = async event => {
             <img src="${imgSrc}" />
             ${movie.Title}
         `;
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+        });
 
         resultsWrapper.appendChild(option);
         
@@ -54,3 +63,10 @@ const onInput = async event => {
 
 // Event listener for input HTML field
 input.addEventListener('input', debounce(onInput, 500));
+
+// Event listener
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
